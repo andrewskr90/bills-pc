9a1f2ffaa3509c1699ae9f21f003703f2d9de3dc
andrewskr90 <andrewskr90@gmail.com>
2021-09-20 16:07:32 -0700

    created path between server and build

--- Back-End/api/server.js
+++ Back-End/api/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const cors = require('cors')
+
 const server = express()
 
 server.use(express.json())
+server.use(cors())
+
+server.get('/api', (req, res) => {
+    res.json({ message: 'Bills PC api!' })
+})
 
 module.exports = server
--- Back-End/index.js
+++ Back-End/index.js
@@ -1,7 +1,17 @@
-const server = require('./api/server')
+require('dotenv').config()
+const express = require('express')
+
+const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
+const server = require('./api/server')
+
+server.use(express.static(path.join(__dirname, '../front-end/build')))
+
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../front-end/build', 'index.html'))
+})
 server.listen(PORT, () => {
     console.log(`listening on localhost: ${PORT}`)
 })
