912f9e86ea8022d6c06ae236df62afb8da048097
andrewskr90 <andrewskr90@gmail.com>
2021-12-07 10:33:25 -0800

    created MyCollection component, imported to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,21 @@
-import React, { useState } from 'react'
-import Search from './components/Search'
-import MyCollection from './components/MyCollection'
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import React, { useState } from 'react';
+import Search from './components/Search';
+import MyCollection from './components/MyCollection';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
-import RipForm from './components/RipForm'
-import PurchaseForm from './components/PurchaseForm'
-import SearchedCardPage from './components/SearchedCardPage'
+import RipForm from './components/RipForm';
+import PurchaseForm from './components/PurchaseForm';
+import SearchedCardPage from './components/SearchedCardPage';
 import HomeNavBar from './HomeNavBar';
 
-const dummyCollection = [
-  {
-    product_id: 1,
-    product_name: 'Hidden Fates Elite Trainer Box',
-    product_type: 'Elite Trainer Box',
-    product_retail_price: 49.99,
-    product_release_date: '2019-10-02',
-    product_description: 'hidden fates etb, including shiny charizard gx',
-    set_id: 11,
-    set_name: 'hidden fates'
-  }
-]
-
 function App() {
+
   return (
     <>
-      <HomeNavBar />
-      <section class='my-collection'>
-        <div class='container'>
-
-        </div>
-      </section>
+      <Router>
+        <HomeNavBar />
+        <MyCollection />
+      </Router>
       {/* <header style={{display:'flex'}}>
         <p>Pokemon Card App</p>
         <nav style={{display:'flex', alignItems:'center', padding:'0 5vw 0 5vw'}}>
