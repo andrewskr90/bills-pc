780ebf04730900c48f897f13a3f459d7a9d961f6
andrewskr90 <andrewskr90@gmail.com>
2022-01-31 16:16:07 -0800

    fixed register middleware so username letter casing is uneffected. More true to original pokemon game

--- api/middlewares/register-middleware.js
+++ api/middlewares/register-middleware.js
@@ -1,17 +1,18 @@
 
 function sanitizeUsername(req, res, next) {
-    //capitalize username
-    let username = req.body.username
-    let capitalizedUsername = "";
-    for (let i = 0; i < username.length; i++) {
-        if (i === 0) {
-        capitalizedUsername += username[i].toUpperCase();
-        } else {
-        capitalizedUsername += username[i].toLowerCase();
+    //remove extra spaces, letter casing uneffected
+    let trimmedUsername = req.body.username.trim()
+    let charArray = trimmedUsername.split('')
+    let singleSpacesUsername = ''
+    for (let i=0; i< charArray.length; i++) {
+        if (charArray[i] === ' ') {
+            if (charArray[i+1] === ' ') {
+                continue
+            }
         }
+        singleSpacesUsername += charArray[i]
     }
-    req.body.username = capitalizedUsername
-    
+    req.body.username = singleSpacesUsername
     next()
 }
 
@@ -19,7 +20,7 @@ function registerVerification(req, res, next) {
     if(!req.body.username || !req.body.password || !req.body.email || !req.body.favoriteGen){
         next({ status: 422, message: 'all form fields required' })
     } else if (req.body.role !== 'Trainer') {
-        next({ status: 422, message: 'user role must be equal to trainer' })
+        next({ status: 422, message: 'user role must be equal to Trainer' })
     } else if(req.body.username.length < 4) {
         next({ status: 422, message: 'Username must be at least 4 characters in length' })
     } else if(req.body.password.length < 7) {
