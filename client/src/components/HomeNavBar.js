78691820dacf3b8c5b018a7cbcd07cc80cdf54a3
andrewskr90 <andrewskr90@gmail.com>
2022-02-15 14:20:50 -0800

    created admin login form, cleaned up app.js in order to think about specific user flows. Began admin login flow, in order to add sets, cards, and products to database

---
+++ client/src/components/HomeNavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+const HomeNavBar = (props) => {
+  const { user } = props;
+  const history = useHistory();
+  const clickBillsPC = () => {
+    history.push('/my-collection')
+  }
+
+    return (
+      <header class='home-nav-bar'>
+        <h1
+          onClick={clickBillsPC}
+        >
+          Bill's PC
+        </h1>
+        <nav>
+          {user ? <a href='logout'>Logout</a> : <a href='login'>Login</a>}
+        </nav>
+      </header>
+    )
+}
+
+export default HomeNavBar;
