c81e9d67bd5cb95b564b4d30e0e103949d1acb4e
andrewskr90 <andrewskr90@gmail.com>
2022-02-24 13:46:11 -0800

    edited middlewares to only check for reqrd objkeys

--- api/middlewares/card-middleware.js
+++ api/middlewares/card-middleware.js
@@ -1,35 +1,14 @@
 function verifyCard(req, res, next) {
-    const { 
-        card_name, 
-        card_number, 
-        card_rarity, 
-        card_image_url, 
-        set_id 
-    } = req.body
+    const { card_name, card_set_id } = req.body
     if (!card_name) {
         next({
             status: 400,
             message: 'Must include data type string, card_name'
         })
-    } else if (!card_number) {
+    } else if (!card_set_id) {
         next({
             status: 400,
-            message: 'Must include set data type int, card_number'
-        })
-    } else if (!card_rarity) {
-        next({
-            status: 400,
-            message: 'Must include data type string, card_rarity'
-        })
-    } else if (!card_image_url) {
-        next({
-            status: 400,
-            message: 'Must include data type string, card_image_url'
-        })
-    } else if (!set_id) {
-        next({
-            status: 400,
-            message: 'Must include foreign key, set_id'
+            message: 'Must include foreign key, card_set_id'
         })
     }
     next()
--- api/middlewares/product-middleware.js
+++ api/middlewares/product-middleware.js
@@ -1,41 +1,9 @@
 function verifyProduct(req, res, next) {
-    const { 
-        product_name,
-        product_type,
-        product_retail_price,
-        product_release_date,
-        product_description,
-        set_id
-    } = req.body
+    const { product_name } = req.body
     if (!product_name) {
         next({
             status: 400,
             message: 'Must include data type string, product_name'
-        })
-    } else if (!product_type) {
-        next({
-            status: 400,
-            message: 'Must include set data type string, product_type'
-        })
-    } else if (!product_retail_price) {
-        next({
-            status: 400,
-            message: 'Must include data type decimal, product_retail_price'
-        })
-    } else if (!product_release_date) {
-        next({
-            status: 400,
-            message: 'Must include data type date, product_release_date'
-        })
-    } else if (!product_description) {
-        next({
-            status: 400,
-            message: 'Must include data type string, product_description'
-        }) 
-    }else if (!set_id) {
-        next({
-            status: 400,
-            message: 'Must include foreign key, set_id'
         })
     }
     next()
