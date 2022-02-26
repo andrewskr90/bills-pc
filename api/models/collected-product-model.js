9ff4d0b3b547229e8f3abdf0ac55f29f9a1d4b5c
andrewskr90 <andrewskr90@gmail.com>
2022-02-26 09:59:17 -0800

    created collected-product model and router

---
+++ api/models/collected-product-model.js
@@ -0,0 +1,37 @@
+const db = require('../data/dbconfig')
+
+const find = () => {
+    return db('collected_products')
+}
+
+const findById = (collected_product_id) => {
+    return db('collected_products').where({ collected_product_id }).first()
+}
+
+function findBy(filter) {
+    return db('collected_products').where(filter).orderBy("collected_product_id");
+  }
+
+const add = async (collected_product) => {
+    const [collected_product_id] = await db('collected_products').insert(collected_product, 'collected_product_id')
+    return findById(collected_product_id)
+}
+
+const update = async (collected_product_id, changes) => {
+    const [updated_collected_product] = await db('collected_products').where({ collected_product_id }).update(changes, '*')
+    return updated_collected_product
+}
+
+const remove = async (collected_product_id) => {
+    const [deleted_collected_product] = await db('collected_products').delete().where({ collected_product_id }).returning(['collected_product_id','collected_product_name'])
+    return deleted_collected_product
+}
+
+module.exports = {
+    find,
+    findById,
+    findBy,
+    add, 
+    update,
+    remove
+}
