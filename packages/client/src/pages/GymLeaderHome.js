fbbe1ca9ee1e814dd143a569e17152363d2c1b33
andrewskr90 <andrewskr90@gmail.com>
2022-08-07 19:06:03 -0700

    gym leader home styles added to support admin use on desktop. TcgPlayerManager deleted, decided a script would be better route.

--- packages/client/src/pages/GymLeaderHome.js
+++ packages/client/src/pages/GymLeaderHome.js
@@ -9,10 +9,12 @@ const GymLeaderHome = () => {
         <div className='gymLeaderHome'>
             <header className='header'>
                 <h1>Gym Leader Admin</h1>
-                <Link className='link' to='card-manager'>Card Manager</Link>
-                <Link className='link' to='trainer-manager'>Trainer Manager</Link>
-                <Link className='link' to='/'>Trainer Site</Link>
-                <Link className='link' to='/logout'>Logout</Link>
+                <div className='navBar'>
+                    <Link className='link' to='card-manager'>Card Manager</Link>
+                    <Link className='link' to='trainer-manager'>Trainer Manager</Link>
+                    <Link className='link' to='/'>Trainer Site</Link>
+                    <Link className='link' to='/logout'>Logout</Link>
+                </div>
             </header>
             <Routes>
                 <Route path='/card-manager/*' element={<CardManager />} />
