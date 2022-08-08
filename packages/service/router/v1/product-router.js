b2796d28c3d09e318844ead5de5240c6d2a03a7a
andrewskr90 <andrewskr90@gmail.com>
2022-08-07 19:20:03 -0700

    renamed insertCardsAndSets to updateCatalogue. Added products as a type of item to be inserted. Updated sets and cards routes to be compatible with sets and cards v2 tables. Created product route, middleware, and sql queries.

---
+++ packages/service/router/v1/product-router.js
@@ -0,0 +1,29 @@
+const productRouter = require('express').Router()
+const { verifySession, decodeJwt, gymLeaderOnly } = require('../../middleware/auth-middleware')
+const { addProductsMySQL, getProductsBySetIdMySQL } = require('../../db/queries/productQueries')
+const { generateProductIds, formatProductsFromTcgPlayerDetails } = require('../../middleware/product-middleware')
+
+productRouter.get('/set-id/:setId', 
+    verifySession,
+    decodeJwt,
+    getProductsBySetIdMySQL,
+    (req, res, next) => {
+        const results = req.results
+        res.status(200).json(results)
+})
+
+productRouter.post('/',
+    verifySession, 
+    decodeJwt,
+    gymLeaderOnly,
+    generateProductIds,
+    formatProductsFromTcgPlayerDetails,
+    addProductsMySQL,
+    (req, res, next) => {
+        const results = req.results
+        res.status(201).json({
+            data: results
+        })
+})
+
+module.exports = productRouter
--- packages/service/utils/tcgPlayerFormatters.js
+++ packages/service/utils/tcgPlayerFormatters.js
@@ -19,11 +19,26 @@ const tcgPlayerFormatters = {
                 card_v2_name: card.productName,
                 card_v2_number: card.customAttributes.number,
                 card_v2_rarity: card.rarityName,
-                card_v2_tcgplayer_product_id: card.productId
+                card_v2_tcgplayer_product_id: card.productId,
+                card_v2_foil_only: card.foilOnly
             }
             return newCard
         })
         return formattedCards
+    },
+    formatTcgDetailsProducts(products) {
+        const formattedProducts = products.map(product => {
+            const newProduct = {
+                product_id: product.product_id,
+                product_set_id: product.set_v2_id,
+                product_name: product.productName,
+                product_release_date: product.customAttributes.releaseDate,
+                product_description: product.customAttributes.description,
+                product_tcgplayer_product_id: product.productId
+            }
+            return newProduct
+        })
+        return formattedProducts
     }
 }
 
