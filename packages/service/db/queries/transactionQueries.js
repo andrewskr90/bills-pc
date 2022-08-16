dd945a9988c18dba67aad1f8cb38abce7967065c
andrewskr90 <andrewskr90@gmail.com>
2022-08-16 14:21:11 -0700

    Sales transactions were intermittently not persisting in the db. Collected Cards would persist for hours, then on a whim disappear. I realized this was happeneing when the server was being reset. When I found out the cards would persist even after resetting the server, only if the transaction was followed by another transaction, I presumed the latest transaction was not persisting. Adding a commit to the transaction solved the issue.

--- packages/service/db/queries/transactionQueries.js
+++ packages/service/db/queries/transactionQueries.js
@@ -31,6 +31,28 @@ const findTransactionSalesMySQL = async (req, res, next) => {
         } catch (err) {
             return next(err)
         }
+    } else {
+        const queryString = `SELECT * FROM sales
+            LEFT JOIN sale_cards ON sales.sale_id = sale_cards.sale_card_sale_id
+            LEFT JOIN collected_cards ON collected_cards.collected_card_id = sale_cards.sale_card_collected_card_id
+            LEFT JOIN cards ON cards.card_id = collected_cards.collected_card_card_id
+            LEFT JOIN sale_notes ON sale_note_sale_id = sale_id`
+        const query= new Promise((resolve, reject) => {
+            connection.query(queryString, (err, results) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(results)
+                }
+            })
+        })
+        try {
+            req.results = await query
+            return next()
+        } catch (err) {
+            return next(err)
+        }
+
     }
 }
 
@@ -68,6 +90,8 @@ const addTransactionSalesMySQL = async (req, res, next) => {
                         throw new Error(err)
                     }
                 })
+                //commit transaction
+                connection.commit()
                 resolve({
                     message: 'success'
                 })
