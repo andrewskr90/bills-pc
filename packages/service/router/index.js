1a3d92951e7c85612e3f23ed847696d5858892dc
andrewskr90 <andrewskr90@gmail.com>
2022-09-26 12:13:08 -0700

    added healthcheck route for aws target groups to hit

--- packages/service/router/index.js
+++ packages/service/router/index.js
@@ -2,5 +2,8 @@ const router = require('express').Router()
 const v1Router = require('./v1')
 
 router.use('/v1', v1Router)
+router.get('/healthcheck', (req, res, next) => {
+    res.status(200).json({ status: 'ok' })
+})
 
 module.exports = router
