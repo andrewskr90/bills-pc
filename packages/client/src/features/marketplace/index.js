f0e4e04c007647ec0c814871ec41a66941c4d8c4
andrewskr90 <andrewskr90@gmail.com>
2022-10-03 23:54:48 -0700

    added SetFilters and SetFilterModal components. Within the modal card rarities can be selected. Active filters render in the SetFilters component

--- packages/client/src/features/marketplace/index.js
+++ packages/client/src/features/marketplace/index.js
@@ -3,21 +3,39 @@ import MarketplaceItems from './MarketplaceItems'
 import './assets/marketplace.less'
 import RangeSelectors from './RangeSelectors'
 import SetSelector from './SetSelector'
-
+import SetFilters from './SetFilters'
+import FilterModal from './FilterModal'
 
 const Marketplace = (props) => {
-    const { marketData, setMarketData } = props
+    const { marketData, setMarketData, referenceData } = props
+    const [showFilterModal, setShowFilterModal] = useState(false)
+
+    const openFilterModal = () => {
+        setShowFilterModal(true)
+    }
 
     return (<div className='marketplace'>
         {marketData.sets.length > 0 
-        ?
+        ?        
         <>  
             <SetSelector marketData={marketData} setMarketData={setMarketData} />
+            <SetFilters 
+                marketData={marketData} 
+                setMarketData={setMarketData} 
+                openFilterModal={openFilterModal}
+            />
             <RangeSelectors 
                 marketData={marketData} 
                 setMarketData={setMarketData} 
             />
             <MarketplaceItems marketData={marketData} />
+            <FilterModal 
+                marketData={marketData} 
+                setMarketData={setMarketData} 
+                referenceData={referenceData} 
+                showFilterModal={showFilterModal} 
+                setShowFilterModal={setShowFilterModal}
+            />
         </>
         :
         <p>Loading...</p>}
