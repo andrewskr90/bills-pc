6c0ad15d62c22c3531e9489e92d3903f70ad1d20
andrewskr90 <andrewskr90@gmail.com>
2022-10-04 08:14:03 -0700

    Reorganized UI of marketplace dash, which required moving removeFilter function to marketplace/indexx

--- packages/client/src/features/marketplace/SetFilters.js
+++ packages/client/src/features/marketplace/SetFilters.js
@@ -1,36 +1,10 @@
 import React from 'react'
-import SetFilter from './SetFilter'
 
 const SetFilters = (props) => {
-    const { marketData, setMarketData, openFilterModal } = props
-
-    const removeFilter = (filterToRemove) => {
-        const updatedFilters = marketData.filters.filter(filterToCompare => {
-            let keyToCompare = Object.keys(filterToCompare)[0]
-            let valueToCompare = filterToCompare[keyToCompare]
-            let keyToRemove = Object.keys(filterToRemove)[0]
-            let valueToRemove = filterToRemove[keyToRemove]
-            if (keyToCompare !== keyToRemove) {
-                return filterToCompare
-            } else {
-                if (valueToCompare !== valueToRemove) {
-                    return filterToCompare
-                }
-            }
-        })
-        setMarketData({
-            ...marketData,
-            filters: updatedFilters
-        })
-    }
+    const { openFilterModal } = props
 
     return (<div className='setFilters'>
         <button className='addFilter' onClick={openFilterModal}>Filters</button>
-        <div className='filterBubbles'>
-            {marketData.filters.map((filter, idx) => {
-                return <SetFilter key={idx} filter={filter} removeFilter={removeFilter} />
-            })}
-        </div>
     </div>)
 }
 
--- packages/client/src/features/marketplace/index.js
+++ packages/client/src/features/marketplace/index.js
@@ -4,6 +4,7 @@ import './assets/marketplace.less'
 import RangeSelectors from './RangeSelectors'
 import SetSelector from './SetSelector'
 import SetFilters from './SetFilters'
+import SetFilter from './SetFilter'
 import FilterModal from './FilterModal'
 
 const Marketplace = (props) => {
@@ -14,16 +15,43 @@ const Marketplace = (props) => {
         setShowFilterModal(true)
     }
 
+    const removeFilter = (filterToRemove) => {
+        const updatedFilters = marketData.filters.filter(filterToCompare => {
+            let keyToCompare = Object.keys(filterToCompare)[0]
+            let valueToCompare = filterToCompare[keyToCompare]
+            let keyToRemove = Object.keys(filterToRemove)[0]
+            let valueToRemove = filterToRemove[keyToRemove]
+            if (keyToCompare !== keyToRemove) {
+                return filterToCompare
+            } else {
+                if (valueToCompare !== valueToRemove) {
+                    return filterToCompare
+                }
+            }
+        })
+        setMarketData({
+            ...marketData,
+            filters: updatedFilters
+        })
+    }
+
     return (<div className='marketplace'>
         {marketData.sets.length > 0 
         ?        
         <>  
-            <SetSelector marketData={marketData} setMarketData={setMarketData} />
-            <SetFilters 
-                marketData={marketData} 
-                setMarketData={setMarketData} 
-                openFilterModal={openFilterModal}
-            />
+            <div className='selectorAndFilter'>
+                <SetSelector marketData={marketData} setMarketData={setMarketData} />
+                <SetFilters 
+                    marketData={marketData} 
+                    setMarketData={setMarketData} 
+                    openFilterModal={openFilterModal}
+                />
+            </div>
+            <div className='filterBubbles'>
+                {marketData.filters.map((filter, idx) => {
+                    return <SetFilter key={idx} filter={filter} removeFilter={removeFilter} />
+                })}
+            </div>
             <RangeSelectors 
                 marketData={marketData} 
                 setMarketData={setMarketData} 
