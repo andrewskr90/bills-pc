9fa46dffd4a8567210ce090e4915fbd529abba44
andrewskr90 <andrewskr90@gmail.com>
2022-10-09 19:27:42 -0700

    downloaded chart.js and react-chartjs-2 dependencies. Created MarketplaceChart component, rendering line graph of market prices based on specified dateRange. MarketplaceChart component rendered in MarketplaceItem. Chart and value placed in div for styling purposes, and marketplace.less updated to style marketplace item components

---
+++ packages/client/src/features/marketplace/MarketplaceChart.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import ChartJS from 'chart.js/auto'
+import { Line } from 'react-chartjs-2'
+
+const MarketplaceChart = (props) => {
+    const { item, marketData } = props
+    const itemId = item.card_id || item.product_id
+    let rangeKey
+    if (marketData.dateRange === '1W') {
+        rangeKey = 'week'
+    } else if (marketData.dateRange === '2W') {
+        rangeKey = 'twoWeek'
+    } else if (marketData.dateRange === '1M') {
+        rangeKey = 'month'
+    }
+    
+    const itemPercentChange = Number(item.percentChange)
+
+
+    return (<div className='marketplaceChart'>
+        <Line 
+            datasetIdKey={itemId}
+            data={{
+                labels: item.formattedPrices.prices[rangeKey].map(price => price[0]).reverse(),
+                datasets: [{
+                    id: itemId,
+                    // label: selectedDataType,
+                    data: item.formattedPrices.prices[rangeKey].map(price => price[1]).reverse(),
+                    spanGaps: true,
+                    tension: 0.3,
+                    radius: 0,
+                    borderColor: itemPercentChange > 0 ? 'rgb(0, 200, 0)' : itemPercentChange ? 'rgb(220, 0, 0)' : 'rgb(96, 101, 203)',
+                    borderWidth: '2'
+                    
+                    // color: 'rgb(11, 113, 196)'
+                }] 
+            }}
+            options={{
+                layout: {
+                    padding: -20
+                },
+                plugins: {
+                    legend: { display: false },
+                },
+                maintainAspectRatio: false,
+                scales: {
+                    y: {
+                            grid: {
+                                display: false,
+                                drawBorder: false
+                            },
+                            ticks: {
+                                display: false
+                            }
+                        },
+                    x: {
+                        grid: {
+                            display: false,
+                            drawBorder: false
+                        },
+                        ticks: {
+                            display: false
+                        }
+                    }
+                }
+            }}
+        />
+    </div>)
+}
+
+export default MarketplaceChart
--- packages/client/src/features/marketplace/MarketplaceItem.js
+++ packages/client/src/features/marketplace/MarketplaceItem.js
@@ -1,19 +1,21 @@
 import React from 'react'
+import MarketplaceChart from './MarketplaceChart'
 
 const MarketplaceItem = (props) => {
     const { item, marketData, percentChange, itemValue } = props
     
-    return (<div className={`marketplaceItem ${Number(item.percentChange) > 0 ? 'up' : Number(item.percentChange) ? 'down' : ''}`}>
+    const itemPercentChange = Number(item.percentChange)
+
+    return (<div className={`marketplaceItem ${itemPercentChange > 0 ? 'up' : itemPercentChange ? 'down' : ''}`}>
         <div className='image'>
             <img src={`https://product-images.tcgplayer.com/fit-in/656x656/${item.tcgplayer_product_id}.jpg`}></img>
         </div>
-        <div className='setAndItemName'>
-            <p className='setName'>{marketData.sets[marketData.selectedSetIndex].name}</p>
-            <p className='itemName'>{item.name}</p>
-        </div>
-        <div className='valueAndChange'>
-            <p>${itemValue}</p>
-            <p>{percentChange > 0 ? '+' : ''}{percentChange}%</p>
+        <div className='chartAndValue'>
+            <MarketplaceChart item={item} marketData={marketData} />
+            <div className='valueAndChange'>
+                <p>${itemValue}</p>
+                <p>{percentChange > 0 ? '+' : ''}{percentChange}%</p>
+            </div>
         </div>
     </div>)
 }
