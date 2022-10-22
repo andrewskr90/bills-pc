b8a06b40f407ce9724c84d580d89a2860bdb25b5
andrewskr90 <andrewskr90@gmail.com>
2022-10-22 14:39:53 -0700

    moved getMarketPrices to MarketplaceSetItems component, added sort component and needed updateSetSort function to pass into the sort component.

--- packages/client/src/features/marketplace/MarketplaceSetItems.js
+++ packages/client/src/features/marketplace/MarketplaceSetItems.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import MarketplaceItems from './MarketplaceItems'
 import RangeSelectors from './RangeSelectors'
-import SetSelector from './SetSelector'
 import SetFilters from './SetFilters'
 import SetFilter from './SetFilter'
 import FilterModal from './FilterModal'
+import BillsPcService from '../../api/bills-pc'
+import Sort from '../../components/Sort'
+
+const formatSetMarketData = (marketPrices) => {
+    const formattedSetMarketData = marketPrices.map(item => {
+        return {
+            card_id: item.card_v2_id,
+            name: item.card_v2_name || item.product_name,
+            number: item.card_v2_number,
+            rarity: item.card_v2_rarity,
+            foil_only: item.card_v2_foil_only,
+            product_id: item.product_id,
+            release_date: item.product_release_date,
+            description: item.product_description,
+            market_prices: item.market_price_prices,
+            tcgplayer_product_id: item.tcgplayer_product_id
+        }
+    })
+    return formattedSetMarketData
+}
 
 const MarketplaceSetItems = (props) => {
     const {
@@ -16,9 +36,45 @@ const MarketplaceSetItems = (props) => {
         showFilterModal,
         setShowFilterModal
     } = props
+    const selectedSetId = useParams()['setId']
+
+    const updateSetSort = (updatedSet) => {
+        setMarketData({
+            ...marketData,
+            sets: marketData.sets.map(set => {
+                if (set.id === selectedSetId) {
+                    return updatedSet
+                } else {
+                    return set
+                }
+            })
+        })
+    }
+
+    useEffect(() => {
+        const selectedSetMarketData = marketData.sets.filter((set) => set.id === selectedSetId)[0]
+        if (selectedSetMarketData.items.length === 0) {
+            BillsPcService.getMarketPrices({ set_v2_id: selectedSetId})
+                .then(res => {
+                    setMarketData({
+                    ...marketData,
+                        sets: marketData.sets.map(set => {
+                            if (set.id === selectedSetId) {
+                                return {
+                                    ...set,
+                                    items: formatSetMarketData(res.data)
+                                }
+                            } else {
+                                return set
+                            }
+                        })
+                    })
+                })
+        }
+    }, [])
+
     return (<div className='marketplaceSetItems'>
         <div className='selectorAndFilter'>
-            <SetSelector marketData={marketData} setMarketData={setMarketData} />
             <SetFilters 
                 marketData={marketData} 
                 setMarketData={setMarketData} 
@@ -30,10 +86,13 @@ const MarketplaceSetItems = (props) => {
                 return <SetFilter key={idx} filter={filter} removeFilter={removeFilter} />
             })}
         </div>
-        <RangeSelectors 
-            marketData={marketData} 
-            setMarketData={setMarketData} 
-        />
+        <div className='rangeAndSort'>
+            <RangeSelectors 
+                marketData={marketData} 
+                setMarketData={setMarketData} 
+            />
+            <Sort dataObject={marketData.sets.filter(set => set.id === selectedSetId)[0]} setDataObject={updateSetSort} />
+        </div>
         <MarketplaceItems marketData={marketData} />
         <FilterModal 
             marketData={marketData} 
