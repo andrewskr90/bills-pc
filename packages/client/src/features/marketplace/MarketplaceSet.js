c2365e2ce9b0f71ede49a1fbe7e7142e5a98a8f9
andrewskr90 <andrewskr90@gmail.com>
2022-10-22 14:35:03 -0700

    formatted and styled MarketplaceSet component, rendering unavailable for the value and percentage when necessary, and stock image for set symbol when necessary

--- packages/client/src/features/marketplace/MarketplaceSet.js
+++ packages/client/src/features/marketplace/MarketplaceSet.js
@@ -1,14 +1,42 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import MinimalPokeball from '../../assets/images/minimal-pokeball.png'
 
 const MarketplaceSet = (props) => {
     const { marketDataSet, marketData } = props
+    const navigate = useNavigate()
 
-    let percentChange = marketDataSet.topTenPercentChange[marketData.dateRange]
-    return (<div className={`marketDataSet ${percentChange > 0 ? 'up' : percentChange < 0 ? 'down' : ''}`}>
-        <p>{marketDataSet.name}</p>
+    let topTenAverage = marketDataSet.topTenAverage.today ? `$${marketDataSet.topTenAverage.today.toFixed(2)}` : ''
+    let percentChange
+    if (marketDataSet.topTenPercentChange[marketData.dateRange]) {
+        if (marketDataSet.topTenPercentChange[marketData.dateRange] > 0) {
+            percentChange = `+${marketDataSet.topTenPercentChange[marketData.dateRange].toFixed(2)}%`
+        } else {
+            percentChange = `${marketDataSet.topTenPercentChange[marketData.dateRange].toFixed(2)}%`
+        }
+    } else {
+        if (marketDataSet.topTenPercentChange[marketData.dateRange] === 0) {
+            percentChange = `0%`
+        } else {
+            percentChange = 'Unavailable'
+        }
+    }
+    // if (marketDataSet.topTenAverage.today) formattedTopTenAverage = marketDataSet.topTenAverage.today.toFixed(2)
+    // else formattedTopTenAverage = 'Unavailable'
+    // if (marketDataSet.topTenPercentChange[marketData.dateRange]) formattedPercentChange = marketDataSet.topTenPercentChange[marketData.dateRange]
+    // else formattedPercentChange = 'Unavailable'
+
+    return (<div 
+            className={`marketplaceSet ${marketDataSet.topTenPercentChange[marketData.dateRange] > 0 ? 'up' : marketDataSet.topTenPercentChange[marketData.dateRange] < 0 ? 'down' : ''}`}
+            onClick={() => navigate(marketDataSet.id)}
+        >
+        <div className='setSymbol'>
+            <img src={marketDataSet.ptcgio_id ? `https://images.pokemontcg.io/${marketDataSet.ptcgio_id}/symbol.png` : MinimalPokeball} />
+        </div>
+        <p className='setName'>{marketDataSet.name}</p>
         <div className='valueAndChange'>
-            <p>{marketDataSet.topTenAverage.today}</p>
-            <p>{percentChange}</p>
+            <p className='marketValue'>{topTenAverage}</p>
+            <p className='percentChange'>{percentChange}</p>
         </div>
 
     </div>)
