9f39e60af4f675c4dd40cdd325ae0fe5f90e6770
andrewskr90 <andrewskr90@gmail.com>
2022-11-04 12:38:29 -0700

    committing code for previous comment

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom'
 
 import Login from './pages/Login'
 import Marketplace from './features/marketplace'
@@ -8,11 +8,8 @@ import NavBar from './layouts/NavBar'
 import BillsPcService from './api/bills-pc'
 import './styles/App.less'
 import InDevelopment from './pages/InDevelopment'
-import { initialReferenceDataValues, initialMarketData } from './data/initialData'
+import { initialReferenceDataValues } from './data/initialData'
 
-//calling api before app renders can lead to bugs
-//render app first, call data in useEffect,
-//then set initialData to true
 let initialData = false
 
 const App = () => {
@@ -20,64 +17,91 @@ const App = () => {
     const navigate = useNavigate()
     const [collectedCards, setCollectedCards] = useState([])
     const [referenceData, setReferenceData] = useState(initialReferenceDataValues)
-    const [marketData, setMarketData] = useState(initialMarketData)
+    const [topTenLoaded, setTopTenLoaded] = useState([])
+
+    const location = useLocation()
+
+    const formattedExpansions = (expansions) => {
+        return expansions.map(expansion => {
+            return {
+                ...expansion,
+                items: []
+            }
+        })
+    }
+
+    const updateReferenceDataWithTopTen = (expansionsTopTenData, referenceDataSets) => {
+        const fetchedTopTenLib = {}
+        expansionsTopTenData.forEach((expansion, idx) => {
+            fetchedTopTenLib[expansion.id] = idx
+        })
+        const updatedReferenceData = referenceDataSets.map(expansion => {
+            return {
+                ...expansion,
+                topTenAverage: expansionsTopTenData[fetchedTopTenLib[expansion.set_v2_id]].topTenAverage
+            }
+        })
+        return updatedReferenceData
+    }
 
     useEffect(() => {
         (async () => {
             let fetchedUserClaims
             await BillsPcService.authenticateSession()
-                .then(res => fetchedUserClaims)
+                .then(res => fetchedUserClaims = res.data)
                 .catch(err => console.log(err))
-            let fetchedReferenceData
+            let fetchedExpansions
+            await BillsPcService.getSetsV2()
+                .then(res => fetchedExpansions = res.data)
+                .catch(err => console.log(err))
+            let fetchedRarities
             await BillsPcService.getReferenceData()
-                .then(res => fetchedReferenceData = res.data)
+                .then(res => fetchedRarities = res.data.rarities)
                 .catch(err => console.log(err))
-            let fetchedTopTenMarket
-            await BillsPcService.getMarketPrices({ topTenAverage: true })
-                .then(res => fetchedTopTenMarket = res.data)
-                .catch(err => console.log(err))
-
             setUserClaims(fetchedUserClaims)
-            setReferenceData(fetchedReferenceData)
-            setMarketData({
-                ...marketData,
-                sets: fetchedTopTenMarket.map(set => {
-                    return {
-                        ...set,
-                        // assign expansion items sorting state
-                        sort: {
-                            value: 'marketValue',
-                            direction: 'desc',
-                            values: {
-                                name: {
-                                    defaultDirection: 'asc',
-                                    formatted: 'Name'
-                                },
-                                marketValue: {
-                                    defaultDirection: 'desc',
-                                    formatted: 'Market Value'
-                                },
-                                percentChange: {
-                                    defaultDirection: 'desc',
-                                    formatted: 'Percent Change'}
-                            }
-                        }
-                    }
-                })
+            setReferenceData({
+                ...referenceData,
+                sets: formattedExpansions(fetchedExpansions),
+                rarities: fetchedRarities
             })
             initialData = true
+            await BillsPcService.getMarketPrices({ topTenAverage: true })
+                .then(res => {
+                    setTopTenLoaded(res.data)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
         })()
+        if (location.pathname === '/') {
+            navigate('market')
+        }
     }, [])
 
+    useEffect(() => {
+        if (topTenLoaded.length > 0) {
+            setReferenceData({
+                ...referenceData,
+                topTenCalled: true,
+                sets: updateReferenceDataWithTopTen(topTenLoaded, referenceData.sets)
+            })
+        }
+    }, [topTenLoaded])
+    
     return (<>
-        {marketData.sets.length > 0
+        {initialData
         ?
         <div className='app'>
             <header>
                 <h1>Bill's PC</h1>
             </header>
             <Routes>
-                <Route path='/market/*' element={<Marketplace marketData={marketData} setMarketData={setMarketData} referenceData={referenceData} />} />
+                <Route path='/market/*' 
+                    element={<Marketplace 
+                        referenceData={referenceData} 
+                        setReferenceData={setReferenceData}
+                    />} 
+                />
                 {/* <Route path='/collection' element={<Collection userClaims={userClaims} collectedCards={collectedCards} setCollectedCards={setCollectedCards} />} /> */}
                 <Route path='/collection' element={<InDevelopment />} />
                 <Route path='/login' element={<Login setUserClaims={setUserClaims} />} />
@@ -87,7 +111,7 @@ const App = () => {
         :
         <div className='appLoading'>
             <h1>Bill's PC</h1>
-            <p>loading set data...</p>
+            <div className='loadingGradient'>Loading...</div>
         </div>    
         }
     </>)
