97a948cd3fed152bc8481cf0cfaa852c82bab1c5
andrewskr90 <andrewskr90@gmail.com>
2022-11-04 11:49:02 -0700

    removed developments mode from common webpack

--- packages/client/webpack.common.js
+++ packages/client/webpack.common.js
@@ -3,13 +3,12 @@ const path = require( 'path' )
 const Dotenv = require('dotenv-webpack')
 
 module.exports = {
-    mode: 'developments',
     context: __dirname,
     entry: './src/index.js',
     output: {
         path: path.resolve( __dirname, 'dist' ),
         filename: 'main.js',
-        publicPath: '/',
+        publicPath: '/'
     },
     module: {
         rules: [
