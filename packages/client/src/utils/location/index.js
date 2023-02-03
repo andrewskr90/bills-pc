eeeb1acf9ddf2f26195601b66d0aeba56ca32e5f
andrewskr90 <andrewskr90@gmail.com>
2023-02-03 13:19:17 -0800

    late commit for edit.png used in <ItemsTable />, and utils/location where buildPreviousRoutes() is exported. Deleted <PurchaseItems /> which is now a part of <ImportPurchase />. Created pointer class styles in App.less

--- packages/client/src/utils/location/index.js
+++ packages/client/src/utils/location/index.js
@@ -3,3 +3,14 @@ export const locationPathnameParsed = (location) => {
     parsedPath.shift() // remove initial slash in pathname
     return parsedPath
 }
+
+export const buildPreviousRoute = (location) => {
+    const parsedLocation = locationPathnameParsed(location)
+    parsedLocation.pop(-1)
+    let previousRoute = ''
+    parsedLocation.forEach(routePiece => {
+
+        previousRoute += `/${routePiece}`
+    })
+    return previousRoute
+}
