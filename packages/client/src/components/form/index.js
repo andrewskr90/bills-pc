c53c4405b51aac1e21eae11bd85e36455bbf6826
andrewskr90 <andrewskr90@gmail.com>
2023-03-13 15:33:00 -0700

    created <Form /> component which takes in single prop of config. Config has key rows, which is an array of objects, one for each row. The row object has elements key, which is an array of object, one for each element in row. Element.type can be either input or button, with a title for either label or button text. Element.width is the percentage of the containers width. Inputs take onChange prop, and buttons take onClick prop. All elements take the option styles object. Adjusted marketplace.less and search.less to not have their styles overrided by form.less

---
+++ packages/client/src/components/form/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import './assets/form.less'
+
+const Form = (props) => {
+    const { config } = props
+
+    const buildElement = (elements, idx) => {
+        let styleObj = {}
+        if (elements[idx].styles) styleObj = elements[idx].styles
+        if (elements[idx].width) styleObj.width = `${elements[idx].width}%`
+        if (idx !== elements.length-1) styleObj.paddingRight = '5px'
+        if (elements[idx].type === 'input') {
+            return <div 
+                className='labelInput'
+                style={styleObj}
+            >
+                <label>{elements[idx].title}</label>
+                <input 
+                    id={elements[idx].id}
+                    type='text'
+                    name={elements[idx].name}
+                    value={elements[idx].value}
+                    onChange={elements[idx].onChange}
+                />
+            </div>
+        } else if (elements[idx].type === 'button') {
+            if (elements[idx].onSubmit) {
+                return <button onSubmit={elements[idx].onSubmit} style={styleObj}>{elements[idx].title}</button>
+            } else {
+                return <button onClick={elements[idx].onClick} style={styleObj}>{elements[idx].title}</button>
+            }
+        }
+    }
+
+    return (<form>
+        {config.rows.map((row, i) => {
+            return <div className='row'>
+                {row.elements.map((element, j) => {
+                    return buildElement(config.rows[i].elements, j)
+                })}
+            </div>
+        })}
+    </form>)
+}
+
+export default Form
