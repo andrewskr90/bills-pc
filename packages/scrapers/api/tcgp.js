5223723ef0657629e07ba3bf5ecb2cc44640c45a
andrewskr90 <andrewskr90@gmail.com>
2024-05-11 16:36:03 -0700

    remove dotenv

--- packages/scrapers/api/tcgp.js
+++ packages/scrapers/api/tcgp.js
@@ -1,6 +1,4 @@
-import dotenv from 'dotenv'
 import axios from 'axios'
-dotenv.config()
 // curl --include --request POST \
 // --header "application/x-www-form-urlencoded" \
 // --data-binary "grant_type=client_credentials&client_id=PUBLIC_KEY&client_secret=PRIVATE_KEY" \
