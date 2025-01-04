5a6e23b18f65032350a18687b7e4e6132c232853
andrewskr90 <andrewskr90@gmail.com>
2025-01-04 14:09:26 -0800

    get collectedItem info and transactions

--- packages/service/middleware/transaction-middleware.js
+++ packages/service/middleware/transaction-middleware.js
@@ -1,26 +1,21 @@
 const Transaction = require('../models/Transaction')
+const CollectedItem = require('../models/CollectedItem')
+const Condition = require('../models/Condition')
 
 const getCollectedItemTransactions = async (req, res, next) => {
     const userId = req.claims.user_id
     const collectedItemId = req.params.id
     try {
+        const collecteItemInfo = await CollectedItem.getById(collectedItemId)
         const transactions = await Transaction.getByCollectedCardIdNew(collectedItemId, userId)
-        console.log(transactions)
-        // let count = 0
-        // let countedCollectedItems = false
-        // req.results = {}
-        // req.results.items = portfolio.map(item => {
-        //     if (!countedCollectedItems) {
-        //         if (item.count !== null) {
-        //             count += parseInt(item.count)
-        //             countedCollectedItems = true
-        //         }
-        //     }
-        //     delete item.count
-        //     return item
-        // })
-        // req.results.count = count
-        req.results = transactions
+        const conditions = await Condition.find()
+        // TODO only return acquisition transaction and current state. With count of transactions between
+        // TODO filter transactions based on user permissions (only transactions they participated in)
+        req.results = {
+            ...collecteItemInfo,
+            conditionName: conditions.find(condition => condition.condition_id === collecteItemInfo.appraisals[0][1]).condition_name,
+            transactions
+        }
         next()
     } catch (err) {
         next(err)
